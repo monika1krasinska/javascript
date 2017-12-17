7f7dc4d3329176e07b463cca3b81ef95147153dd
Monika Krasińska <monika.krasinska@o2.pl>
2017-12-17 09:53:04 +0100

    git commit -m "zmienne"

---
+++ pomoce-dydaktyczne/1-javascript/js/2-variables.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/*console.log("Monika");*/
+
+var imie;
+var imieDamskie = "Kaja";
+var wiek = 37;
+
+var kolor ="czerwony";
+
+wiek = 22;
+imie = "Sergio";
+imie = imieDamskie;
+
+console.log (imie);
+console.log (kolor);
+console.log (wiek);
