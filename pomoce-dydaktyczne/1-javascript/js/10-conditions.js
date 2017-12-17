a672bd86f17d2319d062b102baf7d3baadc9e1e6
Monika Krasińska <monika.krasinska@o2.pl>
2017-12-17 15:27:43 +0100

    git commit -m "warunki"

--- pomoce-dydaktyczne/1-javascript/js/10-conditions.js
+++ pomoce-dydaktyczne/1-javascript/js/10-conditions.js
@@ -1,1 +1,25 @@
 "use strict"
+
+var wzrostMateusza =190;
+var wzrostOlgi = 189;
+if (wzrostOlgi > wzrostMateusza) {
+    console.log("Olga jest niższa:)");
+}
+
+if (wzrostOlgi > wzrostMateusza) {
+    console.log("Olga jest wyższa:)")
+}
+var kolor= 'zielony';
+switch (kolor) {
+    case "czerwony":
+        console.log('Kolor czerwony')
+        break;
+    case "zielony":
+        console.log('Kolor zielony')
+        break;
+    case 'niebieski':
+        console.log('Kolor niebieski')
+        break;
+    default:
+        console.log('Inny kolor')
+}
